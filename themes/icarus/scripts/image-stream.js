88223df6ec5e6f35963ba282b534f8ab4f74bb4b
panjiabang <panjiabang@gmail.com>
2016-01-28 00:21:43 +0800

    update

--- themes/icarus/scripts/image-stream.js
+++ themes/icarus/scripts/image-stream.js
@@ -10,7 +10,6 @@ if (hexo.config.image_stream) {
 	for (var key in config) {
 		if (hexo.config.image_stream[key] != null) {
 			config[key] = hexo.config.image_stream[key];
-			console.log(hexo.config.image_stream[key]);
 		}
 	}
 }
@@ -25,22 +24,21 @@ hexo.extend.tag.register('stream', function(args, content){
 	}
 	result += '<div class="hexo-img-stream">';
 	result += '<style type="text/css">';
-	result += '.lazy {display:none;}.hexo-img-stream{width:90%;max-width:1100px;margin:3% auto}div.hexo-img-stream figure{background:#fefefe;box-shadow:0 1px 2px rgba(34,25,25,0.4);margin:0 0.05% 3%;padding:3%;padding-bottom:10px;display:inline-block;max-width:25%}div.hexo-img-stream figure img{border-bottom:1px solid #ccc;padding-bottom:15px;margin-bottom:5px}div.hexo-img-stream figure figcaption{font-size:.9rem;color:#444;line-height:1.5;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;}div.hexo-img-stream small{font-size:1rem;float:right;text-transform:uppercase;color:#aaa}div.hexo-img-stream small a{color:#666;text-decoration:none;transition:.4s color}@media screen and (max-width:750px){.hexo-img-stream{column-gap:0}}';
+	result += '.hexo-image-steam-lazy {display:none;}.hexo-img-stream{width:90%;max-width:1100px;margin:3% auto}div.hexo-img-stream figure{background:#fefefe;box-shadow:0 1px 2px rgba(34,25,25,0.4);margin:0 0.05% 3%;padding:3%;padding-bottom:10px;display:inline-block;max-width:25%}div.hexo-img-stream figure img{border-bottom:1px solid #ccc;padding-bottom:15px;margin-bottom:5px}div.hexo-img-stream figure figcaption{font-size:.9rem;color:#444;line-height:1.5;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;}div.hexo-img-stream small{font-size:1rem;float:right;text-transform:uppercase;color:#aaa}div.hexo-img-stream small a{color:#666;text-decoration:none;transition:.4s color}@media screen and (max-width:750px){.hexo-img-stream{column-gap:0}}';
 	result += '</style>';
 	result += content;
 	result += '</div>';
-	result += '<script type="text/javascript">$(\'img.lazy\').lazyload({ effect:\'fadeIn\' });</script>';
+	result += '<script type="text/javascript">$(\'img.hexo-image-steam-lazy\').lazyload({ effect:\'fadeIn\' });</script>';
 	return result;
 }, {ends: true});
 
 hexo.extend.tag.register('figure', function(args){
-	
 	var imgUrl = args.shift();
 	var title = args.join(' ');
 	var placeholder = config['img_placeholder'];
 
 	var result = '<figure>';
-	result += '<img class="lazy nofancy" src="' + placeholder + '" data-original="' + imgUrl + '"/>';
+	result += '<img class="hexo-image-steam-lazy nofancy" src="' + placeholder + '" data-original="' + imgUrl + '"/>';
 	result += '<noscript><img src="' + imgUrl + '"/></noscript>';
 	result += '<figcaption>' + hexo.render.renderSync({text: title, engine: 'markdown'}).replace(/<p>/, '').replace(/<.p>/, '') + '</figcaption>';
 	result += '</figure>';
